c2e0753204d540ecf0d39244f2e5a7f2c116f86d
Kai Hendry <hendry@webconverger.com>
2016-11-02 15:51:33 +0800

    Getting ready !

--- good/main.go
+++ good/main.go
@@ -16,6 +16,12 @@ func main() {
 	templates := template.Must(template.New("main").ParseGlob("../templates/404/*.html"))
 	templates = template.Must(templates.ParseGlob("../templates/includes/*.html"))
 
-	err := templates.ExecuteTemplate(os.Stdout, "404.html", nil)
+	err := templates.ExecuteTemplate(os.Stdout, "404.html", struct {
+		Title    string
+		Synopsis string
+	}{
+		"Glorious 404 page",
+		"Page was not found",
+	})
 	check(err)
 }
